d0419992fb387e325bdeb18678ef1271475ee1e2
Matus Goljer <dota.keys@gmail.com>
2014-11-05 16:45:49 +0100

    Add abstract property list

--- src/Pulse/Monad/Data.hs
+++ src/Pulse/Monad/Data.hs
@@ -1,9 +1,13 @@
 module Pulse.Monad.Data
        ( Env(..)
        , State(..)
+       , PropList(..)
+       , propListFromRaw
        ) where
 
+import Data.Map as M
 import Pulse.Internal.Context (RawContextPtr)
+import Pulse.Internal.PropList (RawPropListPtr, proplistGets)
 import Pulse.Internal.ThreadedMainLoop (RawThreadedMainLoopPtr)
 
 data Env = Env
@@ -11,6 +15,15 @@ data Env = Env
            , envMainThread :: RawThreadedMainLoopPtr
            }
 
+data Properties = ApplicationName deriving (Eq, Ord, Show)
+
+type PropList = Map Properties (Maybe String)
+
+propListFromRaw :: RawPropListPtr -> IO PropList
+propListFromRaw proplist = do
+  appName <- proplistGets proplist "application.name"
+  return $ M.insert ApplicationName appName .
+    id $ M.empty
 data State = State
              { nSinks :: Int
              }
