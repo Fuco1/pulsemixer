18157de905d322f139a06ee928a21d6bbd6dafb0
Matus Goljer <dota.keys@gmail.com>
2014-11-05 16:47:56 +0100

    Add abstract SinkInput

--- src/Pulse/Monad/Data.hs
+++ src/Pulse/Monad/Data.hs
@@ -1,14 +1,17 @@
 module Pulse.Monad.Data
        ( Env(..)
        , State(..)
+       , SinkInput(..)
        , PropList(..)
        , propListFromRaw
        ) where
 
 import Data.Map as M
+
 import Pulse.Internal.Context (RawContextPtr)
 import Pulse.Internal.PropList (RawPropListPtr, proplistGets)
 import Pulse.Internal.ThreadedMainLoop (RawThreadedMainLoopPtr)
+import Pulse.Internal.Volume (RawCVolume)
 
 data Env = Env
            { envContext :: RawContextPtr
@@ -24,6 +27,13 @@ propListFromRaw proplist = do
   appName <- proplistGets proplist "application.name"
   return $ M.insert ApplicationName appName .
     id $ M.empty
+
+data SinkInput = SinkInput
+                 { sinkInputIndex :: Int
+                 , sinkInputName :: String
+                 , sinkInputProplist :: PropList
+                 } deriving (Eq, Show)
+
 data State = State
              { nSinks :: Int
              }
