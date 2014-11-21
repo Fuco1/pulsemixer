38d4f63d641960f0f26ee400630a485dc612137f
Matus Goljer <dota.keys@gmail.com>
2014-11-21 20:59:41 +0100

    synchronize creating new connection---only return when server is "Ready"

--- src/Pulse/Monad/Connection.hs
+++ src/Pulse/Monad/Connection.hs
@@ -3,7 +3,9 @@ module Pulse.Monad.Connection
        , freeConn
        ) where
 
-import Foreign.Ptr (nullPtr)
+import Foreign.Ptr (FunPtr(..), nullPtr)
+
+import Control.Concurrent.MVar
 
 import Pulse.Monad.Data
 
@@ -11,13 +13,25 @@ import Pulse.Internal.Context
 import Pulse.Internal.MainLoopApi
 import Pulse.Internal.ThreadedMainLoop
 
+notifyOnReady :: MVar () -> IO (FunPtr (RawContextNotifyCallback ()))
+notifyOnReady mvar =
+  wrapRawContextNotifyCallback $ \ctx _ -> do
+    state <- contextGetState ctx
+    case state of
+     ContextReady -> putMVar mvar ()
+     otherwise -> return ()
+
 newConn :: IO Env
 newConn = do
   ml <- threadedMainloopNew
   api <- threadedMainloopGetApi ml
   ctx <- contextNew api (Just "haskell-pa-test")
+  mvar <- newEmptyMVar
+  cb <- notifyOnReady mvar
+  contextSetStateCallback ctx cb Nothing
   contextConnect ctx Nothing 0 nullPtr
   mlStatus <- threadedMainloopStart ml
+  takeMVar mvar
   return (Env ctx ml)
 
 freeConn :: Env -> IO ()
